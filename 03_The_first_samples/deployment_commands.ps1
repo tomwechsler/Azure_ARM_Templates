64a7e4a831a79f59065e4dad06edabc2f845e464
Tom Wechsler <tom.wechsler@outlook.com>
2025-07-07 14:23:05 +0200

    first deployment

--- 03_The_first_samples/deployment_commands.ps1
+++ 03_The_first_samples/deployment_commands.ps1
@@ -4,8 +4,14 @@ Install-Module -Name Az -AllowClobber -Verbose -Force
 #Connect to Azure
 Connect-AzAccount
 
+#List all resource groups
+Get-AzResourceGroup
+
 #Create a new resource group
-New-AzResourceGroup -Name "psdemorg" -location "switzerlandnorth"
+New-AzResourceGroup -Name "psdemorg" -location "westeurope"
 
 #Deploy the armtemplate file
-New-AzResourceGroupDeployment -name "armtemplate" -ResourceGroupName "psdemorg" -TemplateFile "the file name"
+New-AzResourceGroupDeployment -name "armtemplate" -ResourceGroupName "psdemorg" -TemplateFile "./03_01_blanksamplearm.json" -WhatIf
+
+#Delete the resource group
+Remove-AzResourceGroup -Name "psdemorg" -Force
